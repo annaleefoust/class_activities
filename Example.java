513792db44bdea2f4493941ccef3a7b8c1415194
annaleefoust <agf177@msstate.edu>
2025-03-31 16:26:03 -0500

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,45 @@
+package pkg;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number1;
+		short number2;
+		long number3;
+		byte number4;
+		
+
+		
+		number1 = 10;
+		number2 = 20;
+		number3 = 30;
+		number4 = 40;
+		
+
+		
+		int sum;
+		sum = number1 + number2;
+		
+		
+		System.out.println("Sum " + sum);
+		
+		String text1;
+		String text2;
+		
+		
+		text1 = "Your name";
+		text2 = "My name";
+		
+		System.out.println("Name combine: " + text1 + " " + text2);
+		
+		
+		int number = 0;
+		
+		number = 10/3;
+		
+		System.out.println("Type Casing " + number);
+	}
+
+}
